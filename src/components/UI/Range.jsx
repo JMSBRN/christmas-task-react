ba6f3d83b25e666736a7c55333d142de34c24f08
Zakhvai Alex <zakhavai@gmail.com>
2021-12-24 20:38:47 +0300

    feat: update Range component

--- src/components/UI/Range.jsx
+++ src/components/UI/Range.jsx
@@ -3,13 +3,43 @@ import React from 'react'
 const Range = (props) => {
 	return (
 		<div className={props.name}>
-		<span className="control-span">{props.title}</span> 
-		<div className="year-slider-container">
-			<output className="slider-output">{props.inputFirst}</output>
-			<div className="year-slider noUi-target noUi-ltr noUi-horizontal noUi-txt-dir-ltr"><div className="noUi-base"><div className="noUi-connects"><div className="noUi-connect" ></div></div><div className="noUi-origin" ><div className="noUi-handle noUi-handle-lower" data-handle="0" tabindex="0" role="slider" aria-orientation="horizontal" aria-valuemin="1940.0" aria-valuemax="2020.0" aria-valuenow="1940.0" aria-valuetext="1940.00"><div className="noUi-touch-area"></div></div></div><div className="noUi-origin"><div className="noUi-handle noUi-handle-upper" data-handle="1" tabindex="0" role="slider" aria-orientation="horizontal" aria-valuemin="1940.0" aria-valuemax="2020.0" aria-valuenow="2020.0" aria-valuetext="2020.00"><div className="noUi-touch-area"></div></div></div></div></div>
-			<output className="slider-output">{props.inputSecond}</output>
-		</div>          
-	</div>
+			<span className="control-span">{props.title}</span>
+			<div className="year-slider-container">
+				<output className="slider-output">{props.inputFirst}</output>
+				<div className="year-slider noUi-target noUi-ltr noUi-horizontal noUi-txt-dir-ltr">
+					<div className="noUi-base">
+						<div className="noUi-connects">
+							<div className="noUi-connect" >
+							</div>
+						</div>
+						<div className="noUi-origin" >
+							<div className="noUi-handle noUi-handle-lower"
+								data-handle={props.dataHamndler}
+								tabindex={props.tabIndex}
+								role={props.role}
+								aria-orientation={props.ariaOrientation}
+								aria-valuemin={props.ariaValuemin}
+								aria-valuemax={props.ariaValuemax}
+								aria-valuenow={props.ariaValuenow}
+								aria-valuetext={props.ariaValueText}>
+								<div className="noUi-touch-area">
+								</div>
+							</div>
+						</div>
+						<div className="noUi-origin">
+							<div className="noUi-handle noUi-handle-upper"
+								data-handle={props.dataHamndler}
+								tabindex={props.tabIndex}
+								role={props.role}
+								aria-orientation={props.ariaOrientation}
+								aria-valuemin={props.ariaValuemin}
+								aria-valuemax={props.ariaValuemax}
+								aria-valuenow={props.ariaValuenow}
+								aria-valuetext={props.ariaValueText}>
+								<div className="noUi-touch-area"></div></div></div></div></div>
+				<output className="slider-output">{props.inputSecond}</output>
+			</div>
+		</div>
 	)
 }
 
--- src/components/controls/Ranges.jsx
+++ src/components/controls/Ranges.jsx
@@ -5,10 +5,38 @@ const Ranges = () => {
 	return (
 		<div className="range">
 		<div className="controls-title">Фильтры по диапазону</div>
-	 <Range name={"year"} title={"Количество экземпляров:"} inputFirst={1} inputSecond={12}/>
-	 <Range name={"year"} title={"Фильтры по диапазону"} inputFirst={1940} inputSecond={2020}/>
+	 <Range 
+	 name={"count"} 
+	 title={"Количество экземпляров:"}
+	 inputFirst={1} 
+	 inputSecond={12}
+	 dataHandle="0"
+	 tabIndex="0"
+	 role="slider"
+	 ariaOrientation="horizontal"
+	 ariaValuemin="1.0"
+	 ariaValuemax="12.0"
+	 ariaValuenow="1.0"
+	 ariaValueText="1.00"/>
+
+	 <Range 
+	 name={"year"} 
+	 title={"Фильтры по диапазону"} 
+	 inputFirst={1940} 
+	 inputSecond={2020}
+	 dataHandle="0"
+	 tabIndex="0"
+	 role="slider"
+	 ariaOrientation="horizontal"
+	 ariaValuemin="1940.0"
+	 ariaValuemax="2020.0"
+	 ariaValuenow="1940.0"
+	 ariaValueText="1940.00"/>
+	 
 	</div>
 	)
 }
 
 export default Ranges;
+
+<div class="noUi-handle noUi-handle-lower" data-handle="0" tabindex="0" role="slider" aria-orientation="horizontal" aria-valuemin="1.0" aria-valuemax="12.0" aria-valuenow="1.0" aria-valuetext="1.00"><div class="noUi-touch-area"></div></div>
