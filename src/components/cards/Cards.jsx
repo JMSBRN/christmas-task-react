55d9c388cc441dac1e1e3d00439d05e16d89e343
Zakhvai Alex <zakhavai@gmail.com>
2022-01-09 23:09:13 +0300

    feat: update Cards component

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -6,45 +6,13 @@ const Cards = ({filters}) => {
 const [data, setData] = useState(dataAr)
 
 useEffect(()=> {  
-	const sortArr = dataAr.filter(el => {
-		if (
-			 filters.shape.length === 0 &&
-			 filters.color.length === 0 &&
-			 filters.size.length === 0
-			 ) {
-			return data
-		} else {
-			if(
-				filters.shape.includes(el.shape) ||
-				filters.color.includes(el.color) ||
-				filters.size.includes(el.size) 
-			){	
-				return dataAr;
-			}
-		} 
- })
+	const sortArr = dataAr.filter(el => (
+		(filters.shape.includes(el.shape) || filters.shape.length === 0 ) &&
+		(filters.color.includes(el.color) || filters.color.length === 0) &&
+		(filters.size.includes(el.size) || filters.size.length === 0 )
+	))
 
-  const newArr =  sortArr.filter(el => {
-
-		if(
-			el.shape.includes(filters.shape.join(', ')) &&
-			el.color.includes(filters.color.join(', ')) && 
-			el.size.includes(filters.size.join(', ')) 
-			){
-
-				console.log(filters.shape.join(', '));
-				console.log(filters.color.join(', '));
-				console.log(filters.size.join(', '));
-
-			return true
-		}
-	 })
-
-	 console.log(newArr);
-
-
-
- setData(newArr)
+ setData(sortArr)
 },[filters]);
 
 	return (
