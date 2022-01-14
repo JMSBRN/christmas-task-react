4ee200a82f5e75aeee5e9cf4d3c8a4540c456c24
Zakhvai Alex <zakhavai@gmail.com>
2022-01-14 19:34:49 +0300

    feat: update Ranges and Range components

--- src/components/UI/Range.jsx
+++ src/components/UI/Range.jsx
@@ -1,46 +1,16 @@
 import React from 'react'
 
 const Range = (props) => {
+const getValue = (e) => {
+console.log(e.target.value);
+}
 	return (
-		<div className={props.name}>
-			<span className="control-span">{props.title}</span>
-			<div className="year-slider-container">
-				<output className="slider-output">{props.inputFirst}</output>
-				<div className="year-slider noUi-target noUi-ltr noUi-horizontal noUi-txt-dir-ltr">
-					<div className="noUi-base">
-						<div className="noUi-connects">
-							<div className="noUi-connect" >
-							</div>
-						</div>
-						<div className="noUi-origin" >
-							<div className="noUi-handle noUi-handle-lower"
-								data-handle={props.dataHamndler}
-								tabIndex={props.tabIndex}
-								role={props.role}
-								aria-orientation={props.ariaOrientation}
-								aria-valuemin={props.ariaValuemin}
-								aria-valuemax={props.ariaValuemax}
-								aria-valuenow={props.ariaValuenow}
-								aria-valuetext={props.ariaValueText}>
-								<div className="noUi-touch-area">
-								</div>
-							</div>
-						</div>
-						<div className="noUi-origin">
-							<div className="noUi-handle noUi-handle-upper"
-								data-handle={props.dataHamndler}
-								tabIndex={props.tabIndex}
-								role={props.role}
-								aria-orientation={props.ariaOrientation}
-								aria-valuemin={props.ariaValuemin}
-								aria-valuemax={props.ariaValuemax}
-								aria-valuenow={props.ariaValuenow}
-								aria-valuetext={props.ariaValueText}>
-								<div className="noUi-touch-area"></div></div></div></div></div>
-				<output className="slider-output">{props.inputSecond}</output>
-			</div>
+		<div className='slider-container'>
+			<output className="slider-output">{props.min}</output>
+			    <input onChange={getValue} className='slider' type="range" min={props.min} max={props.max} ></input>
+			<output className="slider-output">{props.max}</output>		
 		</div>
 	)
 }
-
 export default Range
+
--- src/components/controls/Ranges.jsx
+++ src/components/controls/Ranges.jsx
@@ -1,42 +1,17 @@
+
 import React from 'react'
 import Range from '../UI/Range';
 
 const Ranges = () => {
 	return (
-		<div className="range">
+	<div className="range">
 		<div className="controls-title">Фильтры по диапазону</div>
-	 <Range 
-	 name={"count"} 
-	 title={"Количество экземпляров:"}
-	 inputFirst={1} 
-	 inputSecond={12}
-	 dataHandle="0"
-	 tabIndex="0"
-	 role="slider"
-	 ariaOrientation="horizontal"
-	 ariaValuemin="1.0"
-	 ariaValuemax="12.0"
-	 ariaValuenow="1.0"
-	 ariaValueText="1.00"/>
-
-	 <Range 
-	 name={"year"} 
-	 title={"Фильтры по диапазону"} 
-	 inputFirst={1940} 
-	 inputSecond={2020}
-	 dataHandle="0"
-	 tabIndex="0"
-	 role="slider"
-	 ariaOrientation="horizontal"
-	 ariaValuemin="1940.0"
-	 ariaValuemax="2020.0"
-	 ariaValuenow="1940.0"
-	 ariaValueText="1940.00"/>
-	 
+			<div>Количество экземпляров:</div>
+			<Range min= "1" max="12"/>
+			<div>Год приобретения:</div>
+			<Range  min= "1940" max="2020"/>
 	</div>
 	)
 }
-
 export default Ranges;
 
-<div class="noUi-handle noUi-handle-lower" data-handle="0" tabindex="0" role="slider" aria-orientation="horizontal" aria-valuemin="1.0" aria-valuemax="12.0" aria-valuenow="1.0" aria-valuetext="1.00"><div class="noUi-touch-area"></div></div>
