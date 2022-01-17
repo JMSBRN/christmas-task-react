1dbf6cebcc9301c074884eec8f5359e1564673e9
Zakhvai Alex <zakhavai@gmail.com>
2022-01-17 20:35:31 +0300

    feat: update Cards component

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -7,14 +7,17 @@ const Cards = () => {
 const [data, setData] = useState(dataAr)
 const filters = useContext(ContextFilters)
 useEffect(()=> {  
-	const sortArr = dataAr.filter(el => (
+	const filteredtArr = dataAr.filter(el => (
 		(filters.shape.includes(el.shape) || filters.shape.length === 0 ) &&
 		(filters.color.includes(el.color) || filters.color.length === 0) &&
 		(filters.size.includes(el.size) || filters.size.length === 0 ) &&
-		(filters.favorite.includes(el.favorite) || filters.favorite.length === 0 )
+		(filters.favorite.includes(el.favorite) || filters.favorite.length === 0 ) &&
+		(filters.valueCount.includes(parseInt(el.count)) || filters.valueCount.length === 0 ) &&
+		(filters.valueYear.includes(parseInt(el.year)) || filters.valueYear.length === 0 ) 
 	))
 
- setData(sortArr)
+ setData(filteredtArr)
+
 },[filters]);
 	return (
 		<div className='cards'>
