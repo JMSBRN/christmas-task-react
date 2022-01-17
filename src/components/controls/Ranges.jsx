93b20437dd7f31cd4737024e4944c97dfeae5c63
Zakhvai Alex <zakhavai@gmail.com>
2022-01-17 16:58:21 +0300

    feat: update Ranges component, delete Range component

--- src/components/controls/Ranges.jsx
+++ src/components/controls/Ranges.jsx
@@ -1,25 +1,55 @@
 
 import React, { useState } from 'react'
-import Range from '../UI/Range'
+import Slider from '@mui/material/Slider';
+import Box from '@mui/material/Box';
 
 const Ranges = () => {
 
+	const [value, setValue] = useState([3, 6])
+	const [value2, setValue2] = useState([1960, 1980])
+
+ function handleChangeCount(event, newValue) {
+	setValue(newValue);
+
+ }
+ function handleChangeYear(event, newValue) {
+	setValue2(newValue);
+
+ }
 	return (
-	<div className="range">
-		<div className="controls-title">Фильтры по диапазону</div>
+		<div className="range">
+			<div className="controls-title">Фильтры по диапазону</div>
 			<div>Количество экземпляров:</div>
 			<div className="count-slider-container">
-				<output className='slider-output'>1</output>
-								<Range/>
-					<output className='slider-output'>12</output>
+				<output className='slider-output'>{value[0]}</output>
+				<Box className='box' sx={{ width: 500 }}>
+            <Slider
+              sx={ {color: '#287471'}}
+              className="slider"
+              getAriaLabel={() => 'Temperature range'}
+              value={value}
+              onChange={handleChangeCount}
+              valueLabelDisplay="off"
+            />
+      </Box>
+				<output className='slider-output'>{value[1]}</output>
 			</div>
 			<div>Год приобретения:</div>
 			<div className="year-slider-container">
-				<output className='slider-output'>1940</output>
-					<Range/>
-				<output className='slider-output'>2020</output>
+				<output className='slider-output'>{value2[0]}</output>
+				<Box className='box' sx={{ width: 500 }}>
+            <Slider
+              sx={ {color: '#287471'}}
+              className="slider"
+              getAriaLabel={() => 'Temperature range'}
+              value={value2}
+              onChange={handleChangeYear}
+              valueLabelDisplay="off"
+            />
+      </Box>
+				<output className='slider-output'>{value2[1]}</output>
 			</div>
-	</div>
+		</div>
 	)
 }
 export default Ranges;
