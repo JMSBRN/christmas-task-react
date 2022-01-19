2149fd63f4c8148017b385f83d628f9ad4dc9756
Zakhvai Alex <zakhavai@gmail.com>
2022-01-19 16:15:36 +0300

    feat: update sound and snow buttons

--- src/components/UI/ShowAndAudio.jsx
+++ src/components/UI/ShowAndAudio.jsx
@@ -1,12 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import url from '../assets/audio/audio.mp3'
+import IsonSoundImg from '../assets/icons/audio.png'
+import IsonSnowImg from '../assets/icons/snow.png'
+
+const useAudio = (url) => {
+  const [audio] = useState(new Audio(url));
+  const [playing, setPlaying] = useState(false);
+  const toggle = () => setPlaying(!playing);
+
+  useEffect(() => {
+    playing ? audio.play() : audio.pause();
+  },
+    [playing]
+  );
+
+  useEffect(() => {
+    audio.addEventListener('ended', () => setPlaying(false));
+    return () => {
+      audio.removeEventListener('ended', () => setPlaying(false));
+    };
+  }, []);
+
+  return [playing, toggle];
+};
+
+
 
 const ShowAndAudio = () => {
-	return (
-		<div class="snow-audio-container menu-container">
-		<div class="audio-control menu-item"></div>
-		<div class="snow-control menu-item"></div>
-	</div>
-	)
-}
+  const [playing, toggle] = useAudio(url);
 
+  function toggleActiveClass(e) {
+    e.target.classList.toggle("active");
+  }
+  return (
+    <div>
+      <div className="snow-audio-container menu-container">
+        <button className="audio-control menu-item" onClick={toggle}>
+          {playing ? "Pause" : "Play"}
+          <img className='sound-img' onClick={toggleActiveClass} src={IsonSoundImg} width={50} alt='sound-img'></img>
+        </button>
+        <button className="snow-control menu-item">
+          <img onClick={toggleActiveClass} className='show-img' src={IsonSnowImg} width={50} alt='snow-img'></img>
+        </button>
+      </div>
+    </div>
+  );
+}
 export default ShowAndAudio
--- src/components/UI/TreeFavoriteDecorated.jsx
+++ src/components/UI/TreeFavoriteDecorated.jsx
@@ -10,5 +10,4 @@ const TreeFavoriteDecorated = () => {
 	</div>
 	)
 }
-
 export default TreeFavoriteDecorated
